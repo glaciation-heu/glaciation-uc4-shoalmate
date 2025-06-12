e1cc2bcd6cc2148b940e957deaa99eebebb0d98a
Alexander Zharinov <alexander.zharinov@hiro-microdatacenters.nl>
2025-06-12 12:26:59 +0200

    Service stub

--- src/shoalmate/main.py
+++ src/shoalmate/main.py
@@ -1,5 +1,17 @@
+from time import sleep
+import logging
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(levelname)s - %(message)s'
+)
+
+
 def main():
-    print("Hello from Shoalmate!")
+    logging.info("Shoalmate started!")
+    while True:
+        sleep(10)
 
 
 if __name__ == "__main__":
