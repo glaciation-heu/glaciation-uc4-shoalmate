3ba9d678d6d6692b76a43f590528d97e794d8faf
Alexander Zharinov <alexander.zharinov@hiro-microdatacenters.nl>
2025-06-25 19:01:03 +0200

    Refactor settings_mock to use env vars

--- src/tests/shoalmate/conftest.py
+++ src/tests/shoalmate/conftest.py
@@ -1,8 +1,25 @@
+import os
+
 import pytest
+
+from shoalmate.settings import Settings, get_settings
 
 
 @pytest.fixture
-def settings_mock(mocker):
-    get_settings_mock = mocker.patch('shoalmate.allocator.get_settings')
-    get_settings_mock.return_value.rank_similarity_threshold = 0.1
-    return get_settings_mock.return_value
+def settings_mock(mocker) -> Settings:
+    env_vars = {
+        "SHOALMATE__CLUSTER_ID": "A",
+        "SHOALMATE__CLUSTER_A__ACCESS_KEY": "test_access_key_a",
+        "SHOALMATE__CLUSTER_A__SECRET_KEY": "test_secret_key_a",
+        "SHOALMATE__CLUSTER_B__ACCESS_KEY": "test_access_key_b",
+        "SHOALMATE__CLUSTER_B__SECRET_KEY": "test_secret_key_b",
+        "SHOALMATE__CLUSTER_C__ACCESS_KEY": "test_access_key_c",
+        "SHOALMATE__CLUSTER_C__SECRET_KEY": "test_secret_key_c",
+    }
+    mocker.patch.dict(os.environ, env_vars, clear=True)
+    settings = Settings()
+
+    # If the test changes Settings objects, it is consistent across the test
+    mocker.patch('shoalmate.allocator.get_settings', return_value=settings)
+
+    return settings
--- src/tests/shoalmate/test_settings.py
+++ src/tests/shoalmate/test_settings.py
@@ -4,27 +4,14 @@ from unittest.mock import patch
 from shoalmate.settings import Settings, ClusterIDEnum
 
 
-def test__init_from_env__initialized():
-    env_vars = {
-        "SHOALMATE__CLUSTER_ID": "A",
-        "SHOALMATE__CLUSTER_A__ACCESS_KEY": "test_access_key_a",
-        "SHOALMATE__CLUSTER_A__SECRET_KEY": "test_secret_key_a",
-        "SHOALMATE__CLUSTER_B__ACCESS_KEY": "test_access_key_b",
-        "SHOALMATE__CLUSTER_B__SECRET_KEY": "test_secret_key_b",
-        "SHOALMATE__CLUSTER_C__ACCESS_KEY": "test_access_key_c",
-        "SHOALMATE__CLUSTER_C__SECRET_KEY": "test_secret_key_c",
-    }
-    with patch.dict(os.environ, env_vars, clear=True):
-        settings = Settings()
-
-        # Verify that settings were read correctly
-        assert settings.cluster_id == ClusterIDEnum.CLUSTER_A
-        assert settings.cluster_a.access_key == "test_access_key_a"
-        assert settings.cluster_a.secret_key == "test_secret_key_a"
-        assert settings.cluster_b.access_key == "test_access_key_b"
-        assert settings.cluster_b.secret_key == "test_secret_key_b"
-        assert settings.cluster_c.access_key == "test_access_key_c"
-        assert settings.cluster_c.secret_key == "test_secret_key_c"
+def test__init_from_env__initialized(settings_mock):
+        assert settings_mock.cluster_id == ClusterIDEnum.CLUSTER_A
+        assert settings_mock.cluster_a.access_key == "test_access_key_a"
+        assert settings_mock.cluster_a.secret_key == "test_secret_key_a"
+        assert settings_mock.cluster_b.access_key == "test_access_key_b"
+        assert settings_mock.cluster_b.secret_key == "test_secret_key_b"
+        assert settings_mock.cluster_c.access_key == "test_access_key_c"
+        assert settings_mock.cluster_c.secret_key == "test_secret_key_c"
 
         # Verify some default value
-        assert settings.cluster_a.port == 80
+        assert settings_mock.cluster_a.port == 80
