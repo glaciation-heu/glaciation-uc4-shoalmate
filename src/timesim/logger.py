86dccce34e6de1b2dfeeeceb5665e2ac0ac79412
Alexander Zharinov <alexander.zharinov@hiro-microdatacenters.nl>
2025-07-16 17:00:51 +0200

    Add nice logs formatting

--- src/timesim/logger.py
+++ src/timesim/logger.py
@@ -1,3 +1,4 @@
+import sys
 from logging import Logger, getLogger, INFO, StreamHandler, Formatter
 from typing import Annotated
 
@@ -8,8 +9,8 @@ def get_logger() -> Logger:
     logger = getLogger("timesim")
     logger.setLevel(INFO)
     if not logger.hasHandlers():
-        handler = StreamHandler()
-        formatter = Formatter('%(levelname)10s   Package "%(name)s". %(message)s')
+        handler = StreamHandler(sys.stdout)
+        formatter = Formatter("%(levelname)10s   %(message)s")
         handler.setFormatter(formatter)
         logger.addHandler(handler)
     return logger
