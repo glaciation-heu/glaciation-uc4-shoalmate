cbda44ff40ca089ee8220ced826c7c08379500e0
ovantzos <ovantzos@gmail.com>
2025-08-22 15:11:38 +0300

    adapted Orchestrator to use the best available cluster

--- src/shoalmate/orchestrator.py
+++ src/shoalmate/orchestrator.py
@@ -4,11 +4,13 @@ from io import BytesIO
 from time import sleep
 from minio.datatypes import Object
 
-from shoalmate.allocator import Allocator
 from shoalmate.clients.minio import get_client, get_cluster_settings
 from shoalmate.settings import get_settings, ClusterIDEnum
 from timesim.schemas import Timesim
 from timesim.timesim import get_timesim_state
+
+from shoalmate.ranker import Ranker, RankerDebugInfo
+from itertools import cycle
 
 
 class Orchestrator:
@@ -17,9 +19,9 @@ class Orchestrator:
     _sleep_time = 10
 
     def __init__(self) -> None:
-        self._allocator = Allocator()
         self._settings = get_settings()
         self._source_client = get_client(self._settings.cluster_id)
+        self._ranker = Ranker()
 
     def run(self) -> None:
         while True:
@@ -32,7 +34,7 @@ class Orchestrator:
     def _list(self) -> list[Object]:
         bucket = self._settings.input_bucket_chunks
         objects = list(self._source_client.list_objects(bucket))
-        objects.sort(key=lambda x: x.object_name)
+        objects.sort(key=lambda x: x.object_name if x.object_name else "")
         return objects
 
     def _get_target_bucket(self, cluster_id: ClusterIDEnum) -> str:
@@ -42,17 +44,42 @@ class Orchestrator:
             out_bucket += f"-{cluster_id.lower()}"
         return out_bucket
 
+    def _get_output_count(self, target_cluster_id: ClusterIDEnum) -> int:
+        client = get_client(target_cluster_id)
+        bucket = self._get_target_bucket(target_cluster_id)
+        return len(list(client.list_objects(bucket)))
+
+    def _is_busy(self, target_cluster_id: ClusterIDEnum) -> bool:
+        count = self._get_output_count(target_cluster_id)
+        return count > get_cluster_settings(target_cluster_id).output_bucket_capacity
+
+    def _get_target(
+        self, time_offset: timedelta
+    ) -> tuple[ClusterIDEnum, str, RankerDebugInfo]:
+        ranks, debug_info = self._ranker.get(time_offset)
+        for cluster in cycle(
+            sorted(ranks, key=lambda k: str(ranks.get(k)), reverse=True)
+        ):
+            if self._is_busy(cluster):
+                continue
+            else:
+                return cluster, self._get_target_bucket(cluster), debug_info
+        logging.info(
+            "Broke out of the get_target loop in the Orchestrator - NOT SUPPOSED TO HAPPEN!"
+        )
+        cluster = max(ranks, key=lambda k: str(ranks.get(k)))
+        return cluster, self._get_target_bucket(cluster), debug_info
+
     def _move(self, obj: Object) -> None:
         timesim = self._get_active_timesim_state()
         time_offset = timedelta(seconds=timesim.virtual_time_sec)  # type: ignore[arg-type]
-        target_cluster_id, debug_info = self._allocator.get_target_cluster(time_offset)
+        target_cluster_id, target_bucket, debug_info = self._get_target(time_offset)
         target_bucket = self._get_target_bucket(target_cluster_id)
         logging.info(
             f"Experiment {timesim.experiment_tag}. "
             f"Moving {obj.object_name} to cluster {target_cluster_id} "
             f"by Green Index line {debug_info.green_index_line_number}."
         )
-        self._wait_if_busy(target_cluster_id)
         target_client = get_client(target_cluster_id)
         response = self._source_client.get_object(
             self._settings.input_bucket_chunks,
@@ -69,19 +96,6 @@ class Orchestrator:
             obj.object_name,  # type: ignore[arg-type]
         )
 
-    def _wait_if_busy(self, target_cluster_id: ClusterIDEnum) -> None:
-        while True:
-            count = self._get_output_count(target_cluster_id)
-            if count > get_cluster_settings(target_cluster_id).output_bucket_capacity:
-                logging.info(
-                    "Wait cluster %s because it has %d unprocessed objects ",
-                    target_cluster_id.value,
-                    count,
-                )
-                sleep(self._sleep_time)
-            else:
-                break
-
     def _get_active_timesim_state(self) -> Timesim:
         """Wait until Timesim is active and return its state."""
         while True:
@@ -94,8 +108,3 @@ class Orchestrator:
                 )
                 sleep(self._sleep_time)
         return state
-
-    def _get_output_count(self, target_cluster_id: ClusterIDEnum) -> int:
-        client = get_client(target_cluster_id)
-        bucket = self._get_target_bucket(target_cluster_id)
-        return len(list(client.list_objects(bucket)))
